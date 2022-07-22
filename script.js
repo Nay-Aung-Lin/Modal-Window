fe016487b0cc7fa22ee556b14d952cd91805cf9d
Nicky <Theoryt8@gmail.com>
2022-07-22 14:13:30 +0630

    Modal-Window

---
+++ script.js
@@ -0,0 +1,26 @@
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+const showModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+const keyDown = document.addEventListener('keydown',function(e){
+    if(e.key === 'Escape' && !modal.classList.add('hidden')){
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+
+    }
+});
+    for(let i = 0; i < btnShowModal.length; i++){
+        btnShowModal[i].addEventListener('click', showModal);
+    }
+
+btnCloseModal.addEventListener('click',closeModal);
+overlay.addEventListener('click',closeModal);
+
